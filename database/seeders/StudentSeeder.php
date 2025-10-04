08521122486afa79f39a96e698a059d8fc3cb61e
Richard <richardwijayaharianto@gmail.com>
2025-10-04 16:07:16 +0700

    Add Three Student Profile Seeder

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -14,13 +14,39 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('students')->insert([
+        DB::table('students')->insert(
+            [
             'StudentId' => '2602078146',
             'StudentFullName' => 'DANIEL EBENEZER BUDIHARTO',
             'Email' => 'daniel.budiharto@binus.ac.id',
             'StudentPassword' => '$2y$12$bpDdu32t9d4yFgnKw4Ujaudfw8ObbOVqaCG0oFiwrDgs/pVKmvAiu',
             'created_at' => now(),
             'updated_at' => now(),
-        ]);
+            ],
+            [
+            'StudentId' => '26020154930',
+            'StudentFullName' => 'RICHARD WIJAYA HARIANTO',
+            'Email' => 'richard.harianto@binus.ac.id',
+            'StudentPassword' => '$2y$12$yb4jkqi3VrG6CL/JSIgsPOBO9xOQpTgw0/k2GtLFBtddOTPbdaKoW',
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],
+            [
+            'StudentId' => '2602173892',
+            'StudentFullName' => 'AGUNG TRIVALDO SAPUTRA',
+            'Email' => 'agung.saputra@binus.ac.id',
+            'StudentPassword' => '$2y$12$vF3vaJTrBbQrJ4mG8U/RAufurIe4uN8W6Xz5/PoTnPZCIdOrs5M12',
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],
+            [
+            'StudentId' => '2602163336',
+            'StudentFullName' => 'DAVIN NAYAKA PANDYA',
+            'Email' => 'davin.pandya@binus.ac.id',
+            'StudentPassword' => '$2y$12$7ysJy3DQUOJB0s7/oaoJC.uq7YmC/P.vlvgIwhUPyrn6RKwUdBHSq',
+            'created_at' => now(),
+            'updated_at' => now(),
+            ]
+        );
     }
 }
